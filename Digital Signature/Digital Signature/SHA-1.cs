63d0b7ffcd73d833be96ad858d01dd9c91471c31
Karolina Dubitskaya <Karolina.D100899@gmail.com>
2017-11-18 22:24:48 +0300

    Added class SHA-1_Hash

---
+++ Digital Signature/Digital Signature/SHA-1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Signature
+{
+    public class SHA_1
+    {
+
+        #region Fields 
+        //   private int HashBitCount = 62; //number of significant bits in the hash
+
+        //   private byte[] Buffer;
+        //   private long Count; //number of bytes in the hash
+        //   private uint[] StateSHA1;
+        //   private uint[] ExpandedBuffer;         
+        #endregion
+
+        #region Constructor
+        public SHA_1()
+        {
+            
+        }
+        #endregion
+
+        #region Methods
+        #region public GetHash(string text). Return SHA-1 hash for string
+        public GetHash(string text)
+        {
+            
+        }
+        #endregion
+        #endregion
+
+    }
+}
---
+++ Digital Signature/Digital Signature/SHA-1_Hash.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Signature
+{
+    class SHA_1_Hash
+    {
+        #region Fields and Consts
+        private const int BYTE_COUNT = 20;
+        private byte[] _Value = null;
+        #endregion
+
+        #region Properties
+        public byte[] Value { get; set; }
+        public string Text
+        {
+            get
+            {
+                if (Value != null)
+                {
+                    string sHash = BitConverter.ToString(Value).Remove('-');
+                    return sHash;
+                }
+                else
+                    return String.Empty;
+                
+            }
+        }
+        public int ByteCount { get; }
+        #endregion
+
+        #region Constructor
+        public SHA_1_Hash(): this(null)
+        { }
+
+        public SHA_1_Hash(byte[] Value)
+        {
+            this.Value = Value;
+            this.ByteCount = BYTE_COUNT;
+        }
+        #endregion
+
+        #region Methods
+        #region public SHA_1_Hash Clone(). Return copy of the hash.
+        public  SHA_1_Hash Clone()
+        {
+            SHA_1_Hash result = new SHA_1_Hash();
+
+            if (this.Value != null)
+            {
+                result.Value = new byte[this.Value.Length];
+                this.Value.CopyTo(result.Value, 0);
+            }
+
+            return result;
+        }
+        #endregion
+        #endregion
+    }
+}
